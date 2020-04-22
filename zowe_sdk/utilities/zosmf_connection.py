326199260d75223ece136431818be6a3632914b0
Guilherme Cartier <gcartier@br.ibm.com>
2020-04-22 12:27:32 -0300

    Included docstrings to all modules
    Signed-off-by: Guilherme Cartier <gcartier@br.ibm.com>

--- zowe_sdk/utilities/zosmf_connection.py
+++ zowe_sdk/utilities/zosmf_connection.py
@@ -1,4 +1,5 @@
-"""
+"""Zowe Python Client SDK.
+
 This program and the accompanying materials are made available under the terms of the
 Eclipse Public License v2.0 which accompanies this distribution, and is available at
 
@@ -11,6 +12,23 @@ Copyright Contributors to the Zowe Project.
 
 
 class ZosmfConnection:
+    """
+    Class used to represent a z/OSMF connection.
+
+    ...
+
+    Attributes
+    ----------
+    zosmf_host
+        The z/OSMF host address
+    zosmf_user
+        The user for the z/OSMF REST API
+    zosmf_password
+        The password for the z/OSMF REST API
+    ssl_verification
+        The value for the requests ssl verification parameter
+    """
+
     def __init__(
         self,
         zosmf_host,
@@ -18,7 +36,7 @@ class ZosmfConnection:
         zosmf_password,
         ssl_verification
     ):
-        """Base class for z/OSMF connection parameters"""
+        """Construct a ZosmfConnection object."""
         self.zosmf_host = zosmf_host
         self.zosmf_user = zosmf_user
         self.zosmf_password = zosmf_password
