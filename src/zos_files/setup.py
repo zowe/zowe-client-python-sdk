22de2505cb95582e087abd54206154e52ebcef31
Guilherme Cartier <gcartier@br.ibm.com>
2020-07-19 17:43:52 -0300

    Included additional information on setup.py and fixed file header typo
    Signed-off-by: Guilherme Cartier <gcartier@br.ibm.com>
    Signed-off-by: Toine Michielse <toine.michielse@broadcom.com>

--- src/zos_files/setup.py
+++ src/zos_files/setup.py
@@ -3,6 +3,14 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='zowe_zos_files_for_zowe_sdk',
     version='0.0.1',
+    description='Zowe Python SDK - z/OS Files package',
+    url="https://github.com/zowe/zowe-client-python-sdk",
+    author="Guilherme Cartier",
+    author_email="gcartier94@gmail.com",
+    license="EPL-2.0",
+    classifiers=[
+        "License :: OSI Approved :: Eclipse Public License v2.0",
+        "Programming Language :: Python :: 3"],
     install_requires=['zowe.core_for_zowe_sdk'],
     packages=find_namespace_packages(include=['zowe.*'])
 )
