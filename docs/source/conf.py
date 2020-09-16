f31e08423e3133629676126f3ad417c6ba858fc5
Guilherme Cartier <gcartier@br.ibm.com>
2020-09-16 09:10:17 -0300

    Updating version on the conf.py
    Signed-off-by: Guilherme Cartier <gcartier@br.ibm.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Contributors to the Zowe Project'
 author = 'Contributors to the Zowe Project'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
