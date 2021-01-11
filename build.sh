446e23b819686e9dc35de650f1b4b80ccdbaeb6b
Toine Michielse <toine.michielse@broadcom.com>
2021-01-11 12:26:06 +0100

    revert
    Signed-off-by: Toine Michielse <toine.michielse@broadcom.com>

--- build.sh
+++ build.sh
@@ -14,20 +14,20 @@ rm -r zos_jobs/build/ zos_jobs/dist/
 rm -r zos_tso/build/ zos_tso/dist/
 rm -r zosmf/build/ zosmf/dist/
 
-py setup.py sdist bdist_wheel
+python3 setup.py sdist bdist_wheel
 
 cd ${src_path}/core
-py setup.py sdist bdist_wheel
+python3 setup.py sdist bdist_wheel
 cd ${src_path}/zos_console
-py setup.py sdist bdist_wheel
+python3 setup.py sdist bdist_wheel
 cd ${src_path}/zos_files
-py setup.py sdist bdist_wheel
+python3 setup.py sdist bdist_wheel
 cd ${src_path}/zos_jobs
-py setup.py sdist bdist_wheel
+python3 setup.py sdist bdist_wheel
 cd ${src_path}/zos_tso
-py setup.py sdist bdist_wheel
+python3 setup.py sdist bdist_wheel
 cd ${src_path}/zosmf
-py setup.py sdist bdist_wheel
+python3 setup.py sdist bdist_wheel
 
 
 
