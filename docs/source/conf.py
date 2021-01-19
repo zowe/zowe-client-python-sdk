5ddfaf676abb30888b4d08d38096e344dd91f84b
Guilherme Cartier <gcartier@br.ibm.com>
2021-01-19 12:07:24 -0300

    Updated release on documentation
    Signed-off-by: Guilherme Cartier <gcartier@br.ibm.com>
    Signed-off-by: aadityasinha-dotcom <aadityasinha009@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Contributors to the Zowe Project'
 author = 'Contributors to the Zowe Project'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
