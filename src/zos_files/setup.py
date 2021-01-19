44fe4133a68a28415476878e1b2c715e56dd7b8f
Guilherme Cartier <gcartier@br.ibm.com>
2021-01-19 12:05:25 -0300

    Updated versions
    Signed-off-by: Guilherme Cartier <gcartier@br.ibm.com>

--- src/zos_files/setup.py
+++ src/zos_files/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='zowe_zos_files_for_zowe_sdk',
-    version='0.0.1',
+    version='0.5.0',
     description='Zowe Python SDK - z/OS Files package',
     url="https://github.com/zowe/zowe-client-python-sdk",
     author="Guilherme Cartier",
