b21abecdf19426c770458d26e2fdf993d6ee58a2
Guilherme Cartier <gcartier@br.ibm.com>
2021-02-22 15:55:31 -0300

    Included basic integration test for Console package
    Signed-off-by: Guilherme Cartier <gcartier@br.ibm.com>

---
+++ tests/integration/test_zos_console.py
@@ -0,0 +1,19 @@
+from decouple import config
+import unittest
+from zowe.zos_console_for_zowe_sdk import Console
+
+class TestConsoleIntegration(unittest.TestCase):
+    """Console class integration tests."""
+
+    def setUp(self):
+        """Setup fixtures for Console class."""
+        test_profile = config('ZOWE_TEST_PROFILE')
+        self.connection_dict = {"plugin_profile": test_profile}
+        self.console = Console(self.connection_dict)
+    
+    def test_console_command_time_should_return_time(self):
+        """Test the execution of the time command should return the current time"""
+        command_output = self.console.issue_command("D T")
+        self.assertTrue(command_output['cmd-response'].strip().startswith("IEE136I"))
+
+
