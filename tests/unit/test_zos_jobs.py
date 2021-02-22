9e499ed71b012e6361f7fba0f9e1adc243cb7e33
Guilherme Cartier <gcartier@br.ibm.com>
2021-02-22 15:01:39 -0300

    Including basic unit tests for Jobs class
    Signed-off-by: Guilherme Cartier <gcartier@br.ibm.com>
    
    Included space
    
    Signed-off-by: Guilherme Cartier <gcartier@br.ibm.com>

--- tests/unit/test_zos_jobs.py
+++ tests/unit/test_zos_jobs.py
@@ -1,4 +1,4 @@
-"""Unit tests for the Zowe Python SDK z/OS z/OS Files package."""
+"""Unit tests for the Zowe Python SDK z/OS z/OS Jobs package."""
 
 # Including necessary paths
 import sys
@@ -6,19 +6,19 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import unittest
-from src.zos_files.zowe.zos_files_for_zowe_sdk import Files
+from src.zos_jobs.zowe.zos_jobs_for_zowe_sdk import Jobs
 
 
-class TestFilesClass(unittest.TestCase):
-    """File class unit tests."""
+class TestJobsClass(unittest.TestCase):
+    """Jobs class unit tests."""
 
     def setUp(self):
-        """Setup fixtures for File class."""
+        """Setup fixtures for Jobs class."""
         self.connection_dict = {"host_url": "https://mock-url.com",
                                 "user": "Username",
                                 "password": "Password"}
 
     def test_object_should_be_instance_of_class(self):
-        """Created object should be instance of Files class."""
-        files = Files(self.connection_dict)
-        self.assertIsInstance(files, Files)
+        """Created object should be instance of Jobs class."""
+        jobs = Jobs(self.connection_dict)
+        self.assertIsInstance(jobs, Jobs)
