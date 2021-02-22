0e496d8639e31cd94a5e710913ba642e5ff29871
Guilherme Cartier <gcartier@br.ibm.com>
2021-02-22 10:42:21 -0300

    Including unit tests for ApiConnection class
    Signed-off-by: Guilherme Cartier <gcartier@br.ibm.com>

---
+++ tests/unit/test_zowe_core.py
@@ -0,0 +1,40 @@
+"""Unit tests for the Zowe Python SDK Core package."""
+
+# Including necessary paths
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import unittest
+from src.core.zowe.core_for_zowe_sdk import ApiConnection
+from src.core.zowe.core_for_zowe_sdk import exceptions
+
+
+class TestApiConnectionClass(unittest.TestCase):
+    """ApiConnection class unit tests."""
+
+    def setUp(self):
+        """Setup ApiConnection fixtures."""
+        self.url = 'https://mock-url.com'
+        self.user = 'Username'
+        self.password = 'Password'
+
+    def test_object_should_be_instance_of_class(self):
+        """Created object should be instance of ApiConnection class."""
+        api_connection = ApiConnection(self.url, self.user, self.password)
+        self.assertIsInstance(api_connection, ApiConnection)
+
+    def test_object_should_raise_custom_error_without_url(self):
+        """Instantiation of ApiConnection object should raise MissingConnectionArgs if host_url is blank."""
+        with self.assertRaises(exceptions.MissingConnectionArgs):
+            ApiConnection(host_url='', user=self.user, password=self.password)
+
+    def test_object_should_raise_custom_error_without_user(self):
+        """Instantiation of ApiConnection object should raise MissingConnectionArgs if user is blank."""
+        with self.assertRaises(exceptions.MissingConnectionArgs):
+            ApiConnection(host_url=self.url, user='', password=self.password)
+
+    def test_object_should_raise_custom_error_without_password(self):
+        """Instantiation of ApiConnection object should raise MissingConnectionArgs if password is blank."""
+        with self.assertRaises(exceptions.MissingConnectionArgs):
+            ApiConnection(host_url=self.url, user=self.user, password='')
