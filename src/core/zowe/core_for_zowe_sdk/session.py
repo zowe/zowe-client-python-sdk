465e65ca489056da0315143241acb21bdf0c551d
Soumik Dutta <shalearkane@gmail.com>
2022-07-18 19:02:42 +0530

    added Session Class
    Signed-off-by: Soumik Dutta <shalearkane@gmail.com>

--- src/core/zowe/core_for_zowe_sdk/session.py
+++ src/core/zowe/core_for_zowe_sdk/session.py
@@ -14,7 +14,7 @@ from dataclasses import dataclass
 from typing import Union
 
 from zowe.core_for_zowe_sdk.zosmf_profile2 import ProfileManager
-from .import session_constants
+from . import session_constants
 
 
 @dataclass
@@ -25,7 +25,7 @@ class ISession:
 
     hostname: str
     port: int
-    rejectUnauthorised: bool
+    rejectUnauthorised: bool = True
     user: Union[str, None] = None
     password: Union[str, None] = None
     protocol: Union[str, None] = None
@@ -41,14 +41,28 @@ class Session:
     Class used to represent connection details
     """
 
-    def __init__(self, session: ISession):
-        self.session = session
-        if self.session.user is not None and self.session.password is not None:
+    def __init__(self, props: dict) -> ISession:
+        # set hostname and port
+        if props["hostname"] is not None and props["port"] is not None:
+            self.session: ISession = ISession(
+                hostname=props["hostname"], port=props["port"]
+            )
+        else:
+            raise "Hostname and Port must be supplied"
+
+        # determine authentication type
+        if props["user"] is not None and props["password"] is not None:
+            self.session.user = props["user"]
+            self.session.password = props["password"]
             self.session.type = session_constants.AUTH_TYPE_BASIC
-        elif self.session.tokenType is not None and self.session.tokenValue is not None:
+        elif props["tokenType"] is not None and props["tokenValue"] is not None:
+            self.session.tokenType = props["tokenType"]
+            self.session.tokenValue = props["tokenValue"]
             self.session.type = session_constants.AUTH_TYPE_TOKEN
         else:
             raise "An authentication method must be supplied"
+
+        return self.session
 
     @property
     def host_url(self) -> str:
