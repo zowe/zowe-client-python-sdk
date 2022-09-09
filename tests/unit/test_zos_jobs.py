f8bbd4b266e0f59c3123e1052320c4e4ea950ab0
Anton Delchev <dlchv.mail@gmail.com>
2022-09-09 15:38:02 +0200

    Directly assign value of modify_version to custom_args; fix indentation in TestJobsClass' setUp method.
    Signed-off-by: Anton Delchev <dlchv.mail@gmail.com>

--- tests/unit/test_zos_jobs.py
+++ tests/unit/test_zos_jobs.py
@@ -9,12 +9,13 @@ class TestJobsClass(TestCase):
 
     def setUp(self):
         """Setup fixtures for Jobs class."""
-        self.test_profile = {"host": "https://mock-url.com",
-                                "user": "Username",
-                                "password": "Password",
-                                "port": 443,
-                                "rejectUnauthorized": True
-                                }
+        self.test_profile = {
+            "host": "https://mock-url.com",
+            "user": "Username",
+            "password": "Password",
+            "port": 443,
+            "rejectUnauthorized": True
+        }
 
     def test_object_should_be_instance_of_class(self):
         """Created object should be instance of Jobs class."""
