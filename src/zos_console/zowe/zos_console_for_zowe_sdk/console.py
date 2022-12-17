580d3693102a9849c97ca25349196bdafffa3641
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-12-17 22:32:42 +0530

    Added method to get a command response
    Signed-off-by: aadityasinha-dotcom <aadityasinha009@gmail.com>

--- src/zos_console/zowe/zos_console_for_zowe_sdk/console.py
+++ src/zos_console/zowe/zos_console_for_zowe_sdk/console.py
@@ -43,3 +43,24 @@ class Console(SdkApi):
         custom_args["json"] = request_body
         response_json = self.request_handler.perform_request("PUT", custom_args)
         return response_json
+
+    def get_response(self, response_key, console=None):
+        """
+        Collect outstanding synchronous z/OS Console response messages.
+        
+        Parameters
+        ----------
+        response_key
+            The command response key from the Issue Command request.
+        console
+            The console that should be used to get the command response.
+        Returns
+        -------
+        json
+            A JSON containing the response to the command
+        """
+        custom_args = self._create_custom_request_arguments()
+        request_url = "{}/solmsgs/{}".format(console or "defcn", response_key)
+        custom_args["url"] = self.request_endpoint.replace("defcn", request_url)
+        response_json = self.request_handler.perform_request("GET", custom_args)
+        return response_json
