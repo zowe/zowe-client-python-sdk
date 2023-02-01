5d8d7aff7e9c4cc8d4ff0801b42bcb2568320f22
aadityasinha-dotcom <aadityasinha009@gmail.com>
2023-02-01 11:53:31 +0530

    added tests for nested config
    Signed-off-by: aadityasinha-dotcom <aadityasinha009@gmail.com>

--- tests/unit/test_zowe_core.py
+++ tests/unit/test_zowe_core.py
@@ -259,6 +259,37 @@ class TestZosmfProfileManager(TestCase):
         self.assertEqual(props, expected_props)
 
     @patch("keyring.get_password", side_effect=keyring_get_password)
+    def test_custom_file_and_custom_profile_loading(self, get_pass_func):
+        """
+        Test loading of correct file given a filename and directory,
+        also load by profile_name correctly populating fields from custom
+        profile and secure credentials
+        """
+        # Setup - copy profile to fake filesystem created by pyfakefs
+        custom_file_path = os.path.join(self.custom_dir, self.custom_filename)
+        shutil.copy(self.original_file_path, custom_file_path)
+
+        self.setUpCreds(custom_file_path, {
+            "profiles.zosmf.properties.user": "user",
+            "profiles.zosmf.properties.password": "password",
+        })
+
+        # Test
+        prof_manager = ProfileManager(appname=self.custom_appname)
+        prof_manager.config_dir = self.custom_dir
+        props: dict = prof_manager.load(profile_name="lpar1.zosmf")
+        self.assertEqual(prof_manager.config_filepath, custom_file_path)
+
+        expected_props = {
+            "host": "zowe.test.cloud",
+            "rejectUnauthorized": False,
+            "user": "user",
+            "password": "password",
+            "port": 10443,
+        }
+        self.assertEqual(props, expected_props)
+
+    @patch("keyring.get_password", side_effect=keyring_get_password)
     def test_profile_loading_with_user_overriden_properties(self, get_pass_func):
         """
         Test overriding of properties from user config,
@@ -279,7 +310,7 @@ class TestZosmfProfileManager(TestCase):
 
         # Test
         prof_manager = ProfileManager()
-        props: dict = prof_manager.load(profile_type="zosmf")
+        props: dict = prof_manager.load(profile_type="lpar1.zosmf")
         self.assertEqual(prof_manager.config_filepath, cwd_up_file_path)
 
         expected_props = {
