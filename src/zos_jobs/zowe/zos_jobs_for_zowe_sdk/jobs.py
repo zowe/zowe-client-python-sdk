ffcf9d90c4b9b31703da4fd9258faa539bdb59ed
aadityasinha-dotcom <aadityasinha009@gmail.com>
2023-06-06 08:08:06 +0530

    added method to change the job class using jobname and jobid
    Signed-off-by: aadityasinha-dotcom <aadityasinha009@gmail.com>

--- src/zos_jobs/zowe/zos_jobs_for_zowe_sdk/jobs.py
+++ src/zos_jobs/zowe/zos_jobs_for_zowe_sdk/jobs.py
@@ -117,6 +117,35 @@ class Jobs(SdkApi):
         response_json = self.request_handler.perform_request("DELETE", custom_args, expected_code=[202, 200])
         return response_json
 
+    def change_jobs_class(self, jobname: str, jobid: str, modify_version="2.0"):
+        """Changes the a job class
+
+        Parameters
+        ----------
+        jobname: str
+            The name of the job
+        jobid: str
+            The job id on JES
+        modify_version: str
+            Default ("2.0") specifies that the request is to be processed synchronously. For asynchronous processing - change the value to "1.0"
+
+        Returns
+        -------
+        response_json
+            A JSON containing the result of the request execution
+        """
+        if modify_version not in ("1.0", "2.0"):
+            raise ValueError('Accepted values for modify_version: "1.0" or "2.0"')
+
+        custom_args = self._create_custom_request_arguments()
+        joburl = "{}/{}".format(jobname, jobid)
+        request_url = "{}{}".format(self.request_endpoint, joburl)
+        custom_args["url"] = request_url
+        custom_args["headers"]["X-IBM-Job-Modify-Version"] = modify_version
+
+        response_json = self.request_handler.perform_request("PUT", custom_args, expected_code=[202, 200])
+        return response_json
+
     def list_jobs(self, owner=None,  prefix="*", max_jobs=1000, user_correlator=None):
         """Retrieve list of jobs on JES based on the provided arguments.
 
