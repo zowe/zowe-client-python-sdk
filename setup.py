296480b686b15c69630ac881081371bab20e5f5f
Timothy Johnson <timothy.johnson@broadcom.com>
2023-06-14 10:03:45 -0400

    Try to find namespace packages
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import sys
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
 sys.path.append("src")
 from _version import __version__
 
 setup(
     name="zowe",
     version=__version__,
-    packages=find_packages(where="src"),
+    packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
 )
