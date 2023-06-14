8944e8400d95eab836a5b182f566493344f69dc0
Timothy Johnson <timothy.johnson@broadcom.com>
2023-06-14 16:11:05 -0400

    Try yet again to fix package structure
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
+import glob
+import os.path
 import sys
-from setuptools import find_namespace_packages, setup
+from setuptools import setup
 sys.path.append("src")
 from _version import __version__
 
+sdk_dirs = [path for path in glob.iglob("src/*") if os.path.isdir(path)]
+packages = ["zowe"]
+package_dirs = {"zowe": "src"}
+for sdk_dir in sorted(sdk_dirs):
+    if not os.path.isfile(os.path.join(sdk_dir, "setup.py")):
+        continue
+    sdk_name = os.path.basename(sdk_dir)
+    pkg_name = f"zowe.{sdk_name}_for_zowe_sdk"
+    packages.append(pkg_name)
+    package_dirs[pkg_name] = os.path.join(sdk_dir, "zowe", f"{sdk_name}_for_zowe_sdk")
 setup(
     name="zowe",
     version=__version__,
-    packages=find_namespace_packages(include="zowe.*"),
-    package_dir={"zowe": "src"},
+    packages=packages,
+    package_dir=package_dirs
 )
