6144fb583a441edaf21dace1a01312c7f8e44e41
Timothy Johnson <timothy.johnson@broadcom.com>
2023-06-20 13:14:59 -0400

    Reuse resolve_sdk_dep method from top-level script
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- src/setup.py
+++ src/setup.py
@@ -14,22 +14,23 @@ def resolve_sdk_dep(sdk_name, version_spec):
     else:
         return f"zowe.{sdk_name}_for_zowe_sdk{version_spec}"
 
-setup(
-    name='zowe',
-    version=__version__,
-    description='Zowe Python SDK',
-    url="https://github.com/zowe/zowe-client-python-sdk",
-    author="Guilherme Cartier",
-    author_email="gcartier94@gmail.com",
-    license="EPL-2.0",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)"],
-    install_requires=[resolve_sdk_dep('zos_console', '==' + __version__),
-                      resolve_sdk_dep('zos_files', '==' + __version__),
-                      resolve_sdk_dep('zos_tso', '==' + __version__),
-                      resolve_sdk_dep('zos_jobs', '==' + __version__),
-                      resolve_sdk_dep('zosmf', '==' + __version__)],
-    py_modules=[]
-)
+if __name__ == "__main__":
+    setup(
+        name='zowe',
+        version=__version__,
+        description='Zowe Python SDK',
+        url="https://github.com/zowe/zowe-client-python-sdk",
+        author="Guilherme Cartier",
+        author_email="gcartier94@gmail.com",
+        license="EPL-2.0",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)"],
+        install_requires=[resolve_sdk_dep('zos_console', '==' + __version__),
+                        resolve_sdk_dep('zos_files', '==' + __version__),
+                        resolve_sdk_dep('zos_tso', '==' + __version__),
+                        resolve_sdk_dep('zos_jobs', '==' + __version__),
+                        resolve_sdk_dep('zosmf', '==' + __version__)],
+        py_modules=[]
+    )
--- src/zosmf/setup.py
+++ src/zosmf/setup.py
@@ -1,20 +1,8 @@
-import os.path
 import sys
-import uuid
 from setuptools import setup, find_namespace_packages
 sys.path.append("..")
 from _version import __version__
-
-src_dir = os.path.realpath(os.path.dirname(os.path.dirname(__file__)))
-
-def resolve_sdk_dep(sdk_name, version_spec):
-    if os.path.exists(os.path.join(src_dir, sdk_name, "zowe")):
-        # Handle building from a Git checkout
-        # Based on https://github.com/lab-cosmo/equistore/blob/master/python/equistore-torch/setup.py#L212
-        sdk_dir = os.path.realpath(os.path.join(src_dir, sdk_name))
-        return f"zowe.{sdk_name}_for_zowe_sdk@file://{sdk_dir}?{uuid.uuid4()}"
-    else:
-        return f"zowe.{sdk_name}_for_zowe_sdk{version_spec}"
+from setup import resolve_sdk_dep
 
 setup(
     name='zowe_zosmf_for_zowe_sdk',
