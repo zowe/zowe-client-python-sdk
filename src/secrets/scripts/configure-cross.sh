924018bc410c93b5a4632d4708d55f85bb35c039
Timothy Johnson <timothy.johnson@broadcom.com>
2023-10-31 16:31:36 -0400

    Try again to fix architectures
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- src/secrets/scripts/configure-cross.sh
+++ src/secrets/scripts/configure-cross.sh
@@ -13,30 +13,31 @@ set_env() {
     export PKG_CONFIG_PATH="$CHROOT$1/pkgconfig"
 }
 
-CROSS_DEB_ARCH=$1
 case "$1" in
     "aarch64")
-        CROSS_DEB_ARCH=arm64
+        install_libs arm64
         set_env "/usr/lib/aarch64-linux-gnu"
         ;;
     "armv7")
-        CROSS_DEB_ARCH=armhf
+        install_libs armhf
         set_env "/usr/lib/arm-linux-gnueabihf"
         ;;
     "ppc64le")
-        CROSS_DEB_ARCH=ppc64el
+        install_libs ppc64el
         set_env "/usr/lib/powerpc64le-unknown-linux-gnu"
         ;;
     "s390x")
+        install_libs s390x
         set_env "/usr/lib/s390x-unknown-linux-gnu"
         ;;
     "x86")
+        install_libs i386
         set_env "/usr/lib/i386-linux-gnu"
         ;;
     "x86_64")
+        install_libs amd64
         set_env "/usr/lib/x86_64-linux-gnu"
         ;;
     *)
         ;;
 esac
-install_libs $CROSS_DEB_ARCH
