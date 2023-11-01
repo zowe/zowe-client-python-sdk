8a92eced13671117ea491530ac49ead9a07be473
Timothy Johnson <timothy.johnson@broadcom.com>
2023-11-01 08:41:17 -0400

    Try using GITHUB_ENV for cross-compile options
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- src/secrets/scripts/configure-cross.sh
+++ src/secrets/scripts/configure-cross.sh
@@ -1,42 +1,31 @@
 #!/bin/bash
 
-# Install libsecret for the architecture we are compiling for
-install_libs() {
-    dpkg --add-architecture $1
-    apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y libsecret-1-dev:$1
-}
-
 # Set environment variables needed for cross-compilation in current shell
 set_env() {
-    export PKG_CONFIG_SYSROOT_DIR="${CHROOT:-/}"
-    export RUSTFLAGS="-L $CHROOT$1 $RUSTFLAGS"
-    export PKG_CONFIG_PATH="$CHROOT$1/pkgconfig"
+    echo "CROSS_DEB_ARCH=$1" >> $GITHUB_ENV
+    echo "PKG_CONFIG_SYSROOT_DIR=\"${CHROOT:-/}\"" >> $GITHUB_ENV
+    echo "RUSTFLAGS=\"-L $CHROOT$2 $RUSTFLAGS\"" >> $GITHUB_ENV
+    echo "PKG_CONFIG_PATH=\"$CHROOT$2/pkgconfig\"" >> $GITHUB_ENV
 }
 
 case "$1" in
     "aarch64")
-        install_libs arm64
-        set_env "/usr/lib/aarch64-linux-gnu"
+        set_env arm64 "/usr/lib/aarch64-linux-gnu"
         ;;
     "armv7")
-        install_libs armhf
-        set_env "/usr/lib/arm-linux-gnueabihf"
+        set_env armhf "/usr/lib/arm-linux-gnueabihf"
         ;;
     "ppc64le")
-        install_libs ppc64el
-        set_env "/usr/lib/powerpc64le-unknown-linux-gnu"
+        set_env ppc64el "/usr/lib/powerpc64le-unknown-linux-gnu"
         ;;
     "s390x")
-        install_libs s390x
-        set_env "/usr/lib/s390x-unknown-linux-gnu"
+        set_env s390x "/usr/lib/s390x-unknown-linux-gnu"
         ;;
     "x86")
-        install_libs i386
-        set_env "/usr/lib/i386-linux-gnu"
+        set_env i686 "/usr/lib/i386-linux-gnu"
         ;;
     "x86_64")
-        install_libs amd64
-        set_env "/usr/lib/x86_64-linux-gnu"
+        set_env x86_64 "/usr/lib/x86_64-linux-gnu"
         ;;
     *)
         ;;
