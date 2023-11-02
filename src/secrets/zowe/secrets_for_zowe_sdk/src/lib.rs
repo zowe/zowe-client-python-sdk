c9775947f9725b17f896351083ddd2cef123adc9
Timothy Johnson <timothy.johnson@broadcom.com>
2023-11-02 14:55:35 -0400

    Make keyring a submodule of secrets sdk
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- src/secrets/zowe/secrets_for_zowe_sdk/src/lib.rs
+++ src/secrets/zowe/secrets_for_zowe_sdk/src/lib.rs
@@ -1,5 +1,5 @@
 use pyo3::exceptions::PyValueError;
-use pyo3::prelude::*;
+use pyo3::{prelude::*, py_run};
 
 extern crate secrets_core;
 use secrets_core::*;
@@ -50,11 +50,19 @@ fn find_credentials(service: String) -> PyResult<Vec<(String, String)>> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn keyring_test(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(get_password, m)?)?;
-    m.add_function(wrap_pyfunction!(set_password, m)?)?;
-    m.add_function(wrap_pyfunction!(delete_password, m)?)?;
-    m.add_function(wrap_pyfunction!(find_password, m)?)?;
-    m.add_function(wrap_pyfunction!(find_credentials, m)?)?;
+fn zowe_secrets_for_zowe_sdk(py: Python, module: &PyModule) -> PyResult<()> {
+    let submodule = PyModule::new(py, "zowe.secrets_for_zowe_sdk.keyring")?;
+    submodule.add_function(wrap_pyfunction!(get_password, submodule)?)?;
+    submodule.add_function(wrap_pyfunction!(set_password, submodule)?)?;
+    submodule.add_function(wrap_pyfunction!(delete_password, submodule)?)?;
+    submodule.add_function(wrap_pyfunction!(find_password, submodule)?)?;
+    submodule.add_function(wrap_pyfunction!(find_credentials, submodule)?)?;
+    // Hack from https://github.com/PyO3/pyo3/issues/1517
+    py_run!(
+        py,
+        submodule,
+        "import sys; sys.modules['zowe.secrets_for_zowe_sdk.keyring'] = submodule"
+    );
+    module.add_submodule(submodule)?;
     Ok(())
 }
