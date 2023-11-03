5eca5709449a3f243e465cc53480c4e4c05f7598
Timothy Johnson <timothy.johnson@broadcom.com>
2023-11-03 11:22:02 -0400

    Restructure secrets sdk as namespace package
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- src/secrets/src/lib.rs
+++ src/secrets/src/lib.rs
@@ -1,5 +1,5 @@
 use pyo3::exceptions::PyValueError;
-use pyo3::{prelude::*, py_run};
+use pyo3::prelude::*;
 
 extern crate secrets_core;
 use secrets_core::*;
@@ -50,19 +50,11 @@ fn find_credentials(service: String) -> PyResult<Vec<(String, String)>> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn zowe_secrets_for_zowe_sdk(py: Python, module: &PyModule) -> PyResult<()> {
-    let submodule = PyModule::new(py, "zowe.secrets_for_zowe_sdk.keyring")?;
-    submodule.add_function(wrap_pyfunction!(get_password, submodule)?)?;
-    submodule.add_function(wrap_pyfunction!(set_password, submodule)?)?;
-    submodule.add_function(wrap_pyfunction!(delete_password, submodule)?)?;
-    submodule.add_function(wrap_pyfunction!(find_password, submodule)?)?;
-    submodule.add_function(wrap_pyfunction!(find_credentials, submodule)?)?;
-    // Hack from https://github.com/PyO3/pyo3/issues/1517
-    py_run!(
-        py,
-        submodule,
-        "import sys; sys.modules['zowe.secrets_for_zowe_sdk.keyring'] = submodule"
-    );
-    module.add_submodule(submodule)?;
+fn keyring(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(get_password, m)?)?;
+    m.add_function(wrap_pyfunction!(set_password, m)?)?;
+    m.add_function(wrap_pyfunction!(delete_password, m)?)?;
+    m.add_function(wrap_pyfunction!(find_password, m)?)?;
+    m.add_function(wrap_pyfunction!(find_credentials, m)?)?;
     Ok(())
 }
