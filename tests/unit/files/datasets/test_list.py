a01dc05080bf1997ece9e937aa4179ee4e8c88f3
pem70 <pem70@pitt.edu>
2024-05-31 12:28:58 -0400

    Add more unit tests
    Signed-off-by: pem70 <pem70@pitt.edu>

--- tests/unit/files/datasets/test_list.py
+++ tests/unit/files/datasets/test_list.py
@@ -27,3 +27,28 @@ class TestFilesClass(TestCase):
         for test_case in test_values:
             Files(self.test_profile).list_dsn(*test_case)
             mock_send_request.assert_called()
+
+    @mock.patch("requests.Session.send")
+    def test_list_members(self, mock_send_request):
+        """Test list members sends request"""
+        self.files_instance = Files(self.test_profile)
+        mock_send_request.return_value = mock.Mock(headers={"Content-Type": "application/json"}, status_code=200)
+        mock_send_request.return_value.json.return_value = {"items": ["MEMBER1", "MEMBER2"]}
+
+        test_cases = [
+            ("MY.PDS", None, None, 1000, "member"),
+            ("MY.PDS", "MEM*", None, 1000, "member"),
+            ("MY.PDS", None, "MEMBER1", 1000, "member"),
+            ("MY.PDS", "MEM*", "MEMBER1", 500, "extended")
+        ]
+
+        for dataset_name, member_pattern, member_start, limit, attributes in test_cases:
+            result = self.files_instance.list_dsn_members(dataset_name, member_pattern, member_start, limit, attributes)
+            self.assertEqual(result, ["MEMBER1", "MEMBER2"])
+            mock_send_request.assert_called()
+
+            prepared_request = mock_send_request.call_args[0][0]
+            self.assertEqual(prepared_request.method, "GET")
+            self.assertIn(dataset_name, prepared_request.url)
+            self.assertEqual(prepared_request.headers["X-IBM-Max-Items"], str(limit))
+            self.assertEqual(prepared_request.headers["X-IBM-Attributes"], attributes)
---
+++ tests/unit/files/datasets/test_write.py
@@ -0,0 +1,28 @@
+import re
+from unittest import TestCase, mock
+
+from zowe.zos_files_for_zowe_sdk import Files, exceptions, Datasets
+
+
+class TestWriteClass(TestCase):
+    """File class unit tests."""
+
+    def setUp(self):
+        """Setup fixtures for File class."""
+        self.test_profile = {
+            "host": "mock-url.com",
+            "user": "Username",
+            "password": "Password",
+            "port": 443,
+            "rejectUnauthorized": True,
+        }
+
+    @mock.patch("requests.Session.send")
+    def test_write(self, mock_send_request):
+        """Test list members sends request"""
+        mock_send_request.return_value = mock.Mock(headers={"Content-Type": "application/json"}, status_code=201)
+
+        Files(self.test_profile).write_to_dsn(dataset_name="ds_name", data="test")
+        mock_send_request.assert_called_once()
+        prepared_request = mock_send_request.call_args[0][0]
+        self.assertEqual(prepared_request.method, "PUT")
