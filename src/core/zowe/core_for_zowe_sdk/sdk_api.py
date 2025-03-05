zowe/zowe-client-python-api

validate existing type annotations
Signed-off-by: Olamide Ojo <peterojoolamide@gmail.com>

src/core/zowe/core_for_zowe_sdk/sdk_api.py
@@ -17,6 +17,7 @@ from . import session_constants
 from .logger import Log
 from .request_handler import RequestHandler
 from .session import ISession, Session
+from typig import Dict, Any, Optional, Type, Union
 
 
 class SdkApi:
@@ -35,7 +36,7 @@ class SdkApi:
         Flag to disable logger
     """
 
-    def __init__(self, profile: dict, default_url: str, logger_name: str = __name__, log: bool = True):
+    def __init__(self, profile: dict[str, Any], default_url: str, logger_name: str = __name__, log: bool = True):
         session = Session(profile)
         self.session: ISession = session.load()
 
@@ -63,23 +64,24 @@ class SdkApi:
         self.request_handler = RequestHandler(self.__session_arguments, logger_name=logger_name)
 
         if self.session.type == session_constants.AUTH_TYPE_BASIC:
-            self._request_arguments["auth"] = (self.session.user, self.session.password)
+            self._request_arguments["auth"] = (self.session.user or "", self.session.password or "")
         elif self.session.type == session_constants.AUTH_TYPE_BEARER:
             self._default_headers["Authorization"] = f"Bearer {self.session.token_value}"
         elif self.session.type == session_constants.AUTH_TYPE_TOKEN:
             self._default_headers["Cookie"] = f"{self.session.token_type}={self.session.token_value}"
         elif self.session.type == session_constants.AUTH_TYPE_CERT_PEM:
-            self.__session_arguments["cert"] = self.session.cert
+            cert: Optional[Union[str, tuple[str, str]]] = self.session.cert
+            self.__session_arguments["cert"] = cert
 
-    def __enter__(self):
+    def __enter__(self) -> "SdkApi":
         """Return the SdkApi instance."""
         return self
 
-    def __exit__(self, exc_type, exception, traceback):
+    def __exit__(self, exc_type: Optional[Type[BaseException]], exception: Optional[BaseException], traceback: Optional[object]) -> None:
         """Delete the request handler before exit."""
         del self.request_handler
 
-    def _create_custom_request_arguments(self) -> dict:
+    def _create_custom_request_arguments(self) -> Dict[str, Any]:
         """
         Create a copy of the default request arguments dictionary.
 
@@ -88,7 +90,7 @@ class SdkApi:
 
         Returns
         -------
-        dict
+        Dict[str, Any]
             A deepcopy of the request_arguments
         """
         return copy.deepcopy(self._request_arguments)

src/zos_console/zowe/zos_console_for_zowe_sdk/response/__init__.py
@@ -10,4 +10,7 @@ SPDX-License-Identifier: EPL-2.0
 Copyright Contributors to the Zowe Project.
 """
 
-from .console import ConsoleResponse, IssueCommandResponse
+from .console_response import ConsoleResponse 
+from .issue_response import IssueCommandResponse
+
+__all__= ["ConsoleResponse", "IssueCommandResponse"]

zowe/zowe-client-python-sdk

src/core/zowe/core_for_zowe_sdk/sdk_api.py
@@ -17,6 +17,7 @@ from . import session_constants
 from .logger import Log
 from .request_handler import RequestHandler
 from .session import ISession, Session
+from typing import Dict, Any, Optional, Type, Union
 
 
 class SdkApi:
@@ -35,7 +36,7 @@ class SdkApi:
         Flag to disable logger
     """
 
-    def __init__(self, profile: dict, default_url: str, logger_name: str = __name__, log: bool = True):
+    def __init__(self, profile: dict[str, Any], default_url: str, logger_name: str = __name__, log: bool = True):
         session = Session(profile)
         self.session: ISession = session.load()
 
@@ -63,23 +64,24 @@ class SdkApi:
         self.request_handler = RequestHandler(self.__session_arguments, logger_name=logger_name)
 
         if self.session.type == session_constants.AUTH_TYPE_BASIC:
-            self._request_arguments["auth"] = (self.session.user, self.session.password)
+            self._request_arguments["auth"] = (self.session.user or "", self.session.password or "")
         elif self.session.type == session_constants.AUTH_TYPE_BEARER:
             self._default_headers["Authorization"] = f"Bearer {self.session.token_value}"
         elif self.session.type == session_constants.AUTH_TYPE_TOKEN:
             self._default_headers["Cookie"] = f"{self.session.token_type}={self.session.token_value}"
         elif self.session.type == session_constants.AUTH_TYPE_CERT_PEM:
-            self.__session_arguments["cert"] = self.session.cert
+            cert: Optional[Union[str, tuple[str, str]]] = self.session.cert
+            self.__session_arguments["cert"] = cert
 
-    def __enter__(self):
+    def __enter__(self) -> "SdkApi":
         """Return the SdkApi instance."""
         return self
 
-    def __exit__(self, exc_type, exception, traceback):
+    def __exit__(self, exc_type: Optional[Type[BaseException]], exception: Optional[BaseException], traceback: Optional[object]) -> None:
         """Delete the request handler before exit."""
         del self.request_handler
 
-    def _create_custom_request_arguments(self) -> dict:
+    def _create_custom_request_arguments(self) -> Dict[str, Any]:
         """
         Create a copy of the default request arguments dictionary.
 
@@ -88,7 +90,7 @@ class SdkApi:
 
         Returns
         -------
-        dict
+        Dict[str, Any]
             A deepcopy of the request_arguments
         """
         return copy.deepcopy(self._request_arguments)

src/zos_console/zowe/zos_console_for_zowe_sdk/response/__init__.py
@@ -10,4 +10,7 @@ SPDX-License-Identifier: EPL-2.0
 Copyright Contributors to the Zowe Project.
 """
 
-from .console import ConsoleResponse, IssueCommandResponse
+from .console_response import ConsoleResponse 
+from .response import IssueCommandResponse
+
+__all__= ["ConsoleResponse", "IssueCommandResponse"]