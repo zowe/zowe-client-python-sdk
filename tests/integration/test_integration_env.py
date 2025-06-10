7757c2ece22b3b75d242e3622ecf281602703115
rohansen856 <rohansen856@gmail.com>
2025-06-11 02:54:43 +0530

    test: updated isolated env test
    Signed-off-by: rohansen856 <rohansen856@gmail.com>

--- tests/integration/test_integration_env.py
+++ tests/integration/test_integration_env.py
@@ -1,17 +1,7 @@
-from pathlib import Path
-
-
 def test_isolated_env_does_not_touch_real_home(isolated_test_env):
-    project_dir = isolated_test_env["project_dir"]
-    global_dir = isolated_test_env["global_dir"]
+    config_path = isolated_test_env["config_path"]
+    config_file = isolated_test_env["config_file"]
 
-    # Copy contets of the actual zowe.config.json as it's necessary for the test
-    real_config_path = Path(__file__).resolve().parent.parent.parent / "zowe.config.json"
-    
-    assert real_config_path.exists(), f"Missing config at {real_config_path}"
-
-    config_file = project_dir / "zowe.config.json"
-    config_file.write_text(real_config_path.read_text())
-
+    assert config_path.exists(), f"Missing config at {config_path}"
     assert config_file.exists()
     assert "profiles" in config_file.read_text()
