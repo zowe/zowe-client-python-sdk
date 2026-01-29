11b984af663727764886821cfed325e04a1e0d67
Uladzislau <leksilonchikk@gmail.com>
2026-01-29 10:01:54 +0100

    GH-375 Unit tests
    Signed-off-by: Uladzislau <leksilonchikk@gmail.com>

--- tests/unit/files/datasets/test_get.py
+++ tests/unit/files/datasets/test_get.py
@@ -20,7 +20,17 @@ class TestGetClass(TestCase):
 
     @mock.patch("requests.Session.send")
     def test_get(self, mock_send_request):
-        """Test list members sends request"""
+        """Test get content sends request"""
+        mock_send_request.return_value = mock.Mock(headers={"Content-Type": "application/json"}, status_code=200)
+
+        Files(self.test_profile).ds.get_content(dataset_name="ds_name")
+        mock_send_request.assert_called_once()
+        prepared_request = mock_send_request.call_args[0][0]
+        self.assertEqual(prepared_request.method, "GET")
+
+    @mock.patch("requests.Session.send")
+    def test_retrieve_content(self, mock_send_request):
+        """Test retrieve content sends a correct request"""
         mock_send_request.return_value = mock.Mock(headers={"Content-Type": "application/json"}, status_code=200)
 
         Files(self.test_profile).ds.retrieve_content(dataset_name="ds_name")
@@ -30,7 +40,18 @@ class TestGetClass(TestCase):
 
     @mock.patch("requests.Session.send")
     def test_binary_get(self, mock_send_request):
-        """Test list members sends request"""
+        """Test get binary content sends request"""
+        mock_send_request.return_value = mock.Mock(headers={"Content-Type": "application/json"}, status_code=200)
+
+        Files(self.test_profile).ds.get_binary_content(dataset_name="ds_name")
+        mock_send_request.assert_called_once()
+        prepared_request = mock_send_request.call_args[0][0]
+        self.assertEqual(prepared_request.method, "GET")
+        self.assertEqual(prepared_request.headers["X-IBM-Data-Type"], "binary")
+
+    @mock.patch("requests.Session.send")
+    def test_retrieve_binary_content(self, mock_send_request):
+        """Test retrieve binary content sends request"""
         mock_send_request.return_value = mock.Mock(headers={"Content-Type": "application/json"}, status_code=200)
 
         Files(self.test_profile).ds.retrieve_content(dataset_name="ds_name", content_type=ContentType.BINARY)
--- tests/unit/files/uss/test_uss.py
+++ tests/unit/files/uss/test_uss.py
@@ -40,10 +40,37 @@ class TestFilesClass(TestCase):
         mock_send_request.assert_called_once()
         prepared_request = mock_send_request.call_args[0][0]
         self.assertEqual(prepared_request.method, "GET")
+    
+    @mock.patch("requests.Session.send")
+    def test_retrieve_content(self, mock_send_request):
+        """Test retrieve USS file content sends request"""
+        mock_send_request.return_value = mock.Mock(headers={"Content-Type": "application/json"}, status_code=200)
+
+        Files(self.test_profile).uss.retrieve_content("uss_name")
+        mock_send_request.assert_called_once()
+        prepared_request = mock_send_request.call_args[0][0]
+        self.assertEqual(prepared_request.method, "GET")
 
     @mock.patch("requests.Session.send")
     def test_get_content_with_encoding(self, mock_send_request):
         """Test get USS file content with the specified encoding"""
+        mock_send_request.return_value = mock.Mock(headers={"Content-Type": "text/plain"}, status_code=200, text="हैलो वर्ल्ड")
+
+        result = Files(self.test_profile).uss.get_content(
+            "/some/test/path",
+            file_encoding="UTF-8",
+            receive_encoding="UTF-8"
+        )
+        mock_send_request.assert_called_once()
+        prepared_request = mock_send_request.call_args[0][0]
+        self.assertEqual(prepared_request.method, "GET")
+        self.assertEqual(prepared_request.headers["X-IBM-Data-Type"], "text;fileEncoding=UTF-8")
+        self.assertEqual(prepared_request.headers["Content-Type"], "text/plain; charset=UTF-8")
+        self.assertEqual(result, "हैलो वर्ल्ड")
+
+    @mock.patch("requests.Session.send")
+    def test_retrieve_content_with_encoding(self, mock_send_request):
+        """Test retrieve USS file content with the specified encoding"""
         mock_send_request.return_value = mock.Mock(headers={"Content-Type": "text/plain"}, status_code=200, text="हैलो वर्ल्ड")
 
         result = Files(self.test_profile).uss.retrieve_content(
@@ -63,6 +90,17 @@ class TestFilesClass(TestCase):
         """Test get USS file content streamed sends request"""
         mock_send_request.return_value = mock.Mock(headers={"Content-Type": "application/json"}, status_code=200)
 
+        Files(self.test_profile).uss.get_content_streamed("uss_name", binary=True)
+        mock_send_request.assert_called_once()
+        prepared_request = mock_send_request.call_args[0][0]
+        self.assertEqual(prepared_request.method, "GET")
+        self.assertEqual(prepared_request.headers["X-IBM-Data-Type"], "binary")
+
+    @mock.patch("requests.Session.send")
+    def test_retrieve_content_streamed(self, mock_send_request):
+        """Test retrieve USS file content streamed sends request"""
+        mock_send_request.return_value = mock.Mock(headers={"Content-Type": "application/json"}, status_code=200)
+
         Files(self.test_profile).uss.retrieve_content("uss_name", content_type=ContentType.BINARY, as_stream=True)
         mock_send_request.assert_called_once()
         prepared_request = mock_send_request.call_args[0][0]
@@ -72,6 +110,27 @@ class TestFilesClass(TestCase):
     @mock.patch("requests.Session.send")
     def test_get_content_streamed_with_encoding(self, mock_send_request):
         """Test get response with content in the specified encoding"""
+        mock_send_request.return_value = mock.Mock(
+            headers={"Content-Type": "application/octet-stream"}, 
+            status_code=200,
+            content="हैलो वर्ल्ड".encode()
+        )
+
+        result = Files(self.test_profile).uss.get_content_streamed(
+            "/some/test/path", 
+            file_encoding="UTF-8",
+            receive_encoding="UTF-8"
+        )
+        mock_send_request.assert_called_once()
+        prepared_request = mock_send_request.call_args[0][0]
+        self.assertEqual(prepared_request.method, "GET")
+        self.assertEqual(prepared_request.headers["X-IBM-Data-Type"], "text;fileEncoding=UTF-8")
+        self.assertEqual(prepared_request.headers["Content-Type"], "text/plain; charset=UTF-8")
+        self.assertEqual(result.content.decode(), "हैलो वर्ल्ड")
+
+    @mock.patch("requests.Session.send")
+    def test_retrieve_content_streamed_with_encoding(self, mock_send_request):
+        """Test retrieve response with content in the specified encoding"""
         mock_send_request.return_value = mock.Mock(
             headers={"Content-Type": "application/octet-stream"}, 
             status_code=200,
@@ -139,6 +198,30 @@ class TestFilesClass(TestCase):
         mock_response.iter_content = mock.Mock(return_value=[bytes("हैलो", "UTF-8"), bytes("वर्ल्ड", "UTF-8")])
         mock_send_request.return_value = mock_response
 
+        Files(self.test_profile).uss.download(
+            "/some/test/path",
+            "/some/test/file",
+            binary=True
+        )
+        mock_send_request.assert_called_once()
+        prepared_request = mock_send_request.call_args[0][0]
+        self.assertEqual(prepared_request.method, "GET")
+        self.assertEqual(prepared_request.headers["X-IBM-Data-Type"], "binary")
+        mock_file.assert_called_once_with('/some/test/file', 'wb', encoding=None)
+        mock_file().write.assert_has_calls([mock.call(bytes("हैलो", "UTF-8")), mock.call(bytes("वर्ल्ड", "UTF-8"))])
+
+    @mock.patch("requests.Session.send")
+    @mock.patch("builtins.open", new_callable=mock.mock_open)
+    def test_perform_download_binary(self, mock_file, mock_send_request):
+        """Test perform a download of a binary USS file"""
+        mock_response = mock.Mock(
+            spec=requests.Response,
+            headers={"Content-Type": "application/octet-stream"},
+            status_code=200
+        )
+        mock_response.iter_content = mock.Mock(return_value=[bytes("हैलो", "UTF-8"), bytes("वर्ल्ड", "UTF-8")])
+        mock_send_request.return_value = mock_response
+
         Files(self.test_profile).uss.perform_download(
             "/some/test/path",
             "/some/test/file",
@@ -183,6 +266,26 @@ class TestFilesClass(TestCase):
         mock_send_request.return_value = mock.Mock(headers={"Content-Type": "text/plain"}, status_code=200)
 
         with pytest.raises(TypeError) as e:
+            Files(self.test_profile).uss.download(
+                "/some/test/path",
+                "/some/test/file",
+                file_encoding="UTF-8",
+                receive_encoding="UTF-8"
+            )
+
+        self.assertIn("Expected Response, got", str(e.value))
+        mock_send_request.assert_called_once()
+        prepared_request = mock_send_request.call_args[0][0]
+        self.assertEqual(prepared_request.method, "GET")
+        self.assertEqual(prepared_request.headers["X-IBM-Data-Type"], "text;fileEncoding=UTF-8")
+        self.assertEqual(prepared_request.headers["Content-Type"], "text/plain; charset=UTF-8")
+
+    @mock.patch("requests.Session.send")
+    def test_perform_download_fail_incorrect_response(self, mock_send_request):
+        """Test perform a download of a USS file fails because an incorrect response object is received"""
+        mock_send_request.return_value = mock.Mock(headers={"Content-Type": "text/plain"}, status_code=200)
+
+        with pytest.raises(TypeError) as e:
             Files(self.test_profile).uss.perform_download(
                 "/some/test/path",
                 "/some/test/file",
@@ -204,6 +307,21 @@ class TestFilesClass(TestCase):
         """Test upload a text USS file"""
         mock_send_request.return_value = mock.Mock(headers={"Content-Type": "application/json"}, status_code=201)
 
+        Files(self.test_profile).uss.upload("/some/test/file", "/some/test/path")
+        mock_send_request.assert_called_once()
+        prepared_request = mock_send_request.call_args[0][0]
+        self.assertEqual(prepared_request.method, "PUT")
+        self.assertEqual(prepared_request.headers["Content-Type"], "text/plain; charset=utf-8")
+        mock_is_file.assert_called_once()
+        mock_file.assert_called_once_with('/some/test/file', 'r', encoding='utf-8')
+
+    @mock.patch("requests.Session.send")
+    @mock.patch("builtins.open", new_callable=mock.mock_open)
+    @mock.patch("os.path.isfile", return_value=True)
+    def test_perform_upload_text(self, mock_is_file, mock_file, mock_send_request):
+        """Test perform an upload of a text USS file"""
+        mock_send_request.return_value = mock.Mock(headers={"Content-Type": "application/json"}, status_code=201)
+
         Files(self.test_profile).uss.perform_upload("/some/test/file", "/some/test/path")
         mock_send_request.assert_called_once()
         prepared_request = mock_send_request.call_args[0][0]
@@ -217,6 +335,26 @@ class TestFilesClass(TestCase):
     @mock.patch("os.path.isfile", return_value=True)
     def test_upload_binary(self, mock_is_file, mock_file, mock_send_request):
         """Test upload a binary USS file"""
+        mock_send_request.return_value = mock.Mock(
+            headers={"Content-Type": "application/octet-stream", "X-IBM-Data-Type": "binary"}, status_code=204
+        )
+
+        binary_data = b"Hello world!"
+        mock_file.return_value.read.return_value = binary_data
+
+        Files(self.test_profile).uss.upload("/some/test/file", "/some/test/path", binary=True)
+        mock_send_request.assert_called_once()
+        prepared_request = mock_send_request.call_args[0][0]
+        self.assertEqual(prepared_request.method, "PUT")
+        self.assertEqual(prepared_request.headers["Content-Type"], "application/octet-stream")
+        mock_is_file.assert_called_once()
+        mock_file.assert_called_once_with('/some/test/file', 'rb')
+
+    @mock.patch("requests.Session.send")
+    @mock.patch("builtins.open", new_callable=mock.mock_open)
+    @mock.patch("os.path.isfile", return_value=True)
+    def test_perform_upload_binary(self, mock_is_file, mock_file, mock_send_request):
+        """Test perform an upload of a binary USS file"""
         mock_send_request.return_value = mock.Mock(
             headers={"Content-Type": "application/octet-stream", "X-IBM-Data-Type": "binary"}, status_code=204
         )
@@ -240,6 +378,18 @@ class TestFilesClass(TestCase):
     @mock.patch("os.path.isfile", return_value=False)
     def test_upload_fail_file_not_found(self, mock_is_file, mock_send_request):
         """Test upload a binary USS file"""
+        mock_send_request.return_value = mock.Mock(headers={"Content-Type": "application/json"}, status_code=201)
+
+        with self.assertRaises(FileNotFound):
+            Files(self.test_profile).uss.upload("/some/test/file", "/some/test/path")
+        mock_send_request.assert_not_called()
+        mock_is_file.assert_called_once()
+        mock_is_file.assert_called_once()
+
+    @mock.patch("requests.Session.send")
+    @mock.patch("os.path.isfile", return_value=False)
+    def test_perform_upload_fail_file_not_found(self, mock_is_file, mock_send_request):
+        """Test perform an upload of a binary USS file"""
         mock_send_request.return_value = mock.Mock(headers={"Content-Type": "application/json"}, status_code=201)
 
         with self.assertRaises(FileNotFound):
